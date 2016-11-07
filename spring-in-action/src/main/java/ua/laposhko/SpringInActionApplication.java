a2b96f1a255cc0851f851dde9321ca7c5aafdfcc
Serhii_Laposhko <Serhii_Laposhko@epam.com>
2016-11-07 18:10:09 +0200

    Initial project set up

---
+++ spring-in-action/src/main/java/ua/laposhko/SpringInActionApplication.java
@@ -0,0 +1,12 @@
+package ua.laposhko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInActionApplication.class, args);
+	}
+}
