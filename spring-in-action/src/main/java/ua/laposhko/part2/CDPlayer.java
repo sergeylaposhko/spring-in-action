77618442ee199036989606ebc433ed2ce5b04974
Serhii_Laposhko <Serhii_Laposhko@epam.com>
2016-11-07 23:03:06 +0200

    part 2.2.2 Constructor autowiring

--- spring-in-action/src/main/java/ua/laposhko/part2/CDPlayer.java
+++ spring-in-action/src/main/java/ua/laposhko/part2/CDPlayer.java
@@ -1,4 +1,23 @@
 package ua.laposhko.part2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CDPlayer {
+
+    private CompactDisk compactDisk;
+
+    @Autowired
+    CDPlayer(CompactDisk compactDisk){
+        this.compactDisk = compactDisk;
+    }
+
+    public CompactDisk getCompactDisk() {
+        return compactDisk;
+    }
+
+    public void setCompactDisk(CompactDisk compactDisk) {
+        this.compactDisk = compactDisk;
+    }
 }
--- spring-in-action/src/test/java/ua/laposhko/part2/CDPlayerTest.java
+++ spring-in-action/src/test/java/ua/laposhko/part2/CDPlayerTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
@@ -15,9 +16,18 @@ public class CDPlayerTest {
     @Autowired
     private CompactDisk compactDisk;
 
+    @Autowired
+    private CDPlayer cdPlayer;
+
+
     @Test
     public void testNotNull() {
         assertNotNull(compactDisk);
+    }
+
+    @Test
+    public void testNotEmpty() {
+        assertTrue(cdPlayer.getCompactDisk() != null);
     }
 
 }
